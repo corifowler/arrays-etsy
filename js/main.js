a11bd9b28108fdc3a08eba9106f60638b10e761b
Cori Fowler <fowler.cori@gmail.com>
2015-10-01 15:53:36 -0400

    Finished prob #2

--- js/main.js
+++ js/main.js
@@ -2,22 +2,53 @@
 
 // Get array of all item prices
 
-var prices = items.map( function(item){
+var prices = items.map(function(item){
   return item.price;
 });
 
-console.log(prices);
-
-
 // Find Average Price
 
-function findAvg(prices) {
-  var sum = prices.reduce(function(preVal, currVal) {
+var sum = prices.reduce(function(preVal, currVal) {
     return (preVal + currVal);
   });
-  var average = (sum / prices.length);
-  return 'The average price is $' + average.toFixed(2) + '.';
-};
+
+var average = (sum / prices.length);
+var averageCon = average.toFixed(2);
+
+var avgString = 'The average price is $' + averageCon + '.'
+
+// Put answer on page
+// var answer1 = document.querySelector('#answer1');
+// var textNode = document.createTextNode(avgString);
+// answer1.appendChild(textNode);
+
+// Items between 14.00 and 18.00 - want item title based on price
+
+var titleWithPrice = [];
+
+var splitByPrice = items.forEach(function(x){
+  if (x.price > 14 && x.price < 18) {
+    titleWithPrice.push(x.title);
+  };
+});
+
+var firstItem = titleWithPrice[0];
+var secondItem = titleWithPrice[1];
+var thirdItem = titleWithPrice[2];
+
+// Put answer on page
+var answer2 = document.querySelector('#answer2');
+var textNode = document.createTextNode(firstItem);
+answer2.appendChild(textNode);
+
+var answer2a = document.querySelector('#answer2a');
+var textNode1 = document.createTextNode(secondItem);
+answer2a.appendChild(textNode1);
+
+var answer2b = document.querySelector('#answer2b')
+var textNode2 = document.createTextNode(thirdItem);
+answer2b.appendChild(textNode2);
+
 
 
 
