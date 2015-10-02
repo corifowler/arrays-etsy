411f10d15b0eab6f5dfef3296fad88e8719a28bc
Cori Fowler <fowler.cori@gmail.com>
2015-10-02 16:11:33 -0400

    Finished CSS Styling

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ var gbpNamePrice = [];
 
 var filterByCurrency = items.forEach(function(x){
   if (x.currency_code === 'GBP') {
-    gbpNamePrice.push(x.title + ' costs' + '£' + x.price);
+    gbpNamePrice.push(x.title + ' costs' + '&amp;#163;' + x.price);
   };
 });
 
